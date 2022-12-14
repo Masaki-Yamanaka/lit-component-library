2b82ccf2c0d82eeae65a1ca3d1f472885f8b290d
masaki_yamanaka <ki.ymnka9@gmail.com>
2022-12-14 15:14:53 +0900

    add type

--- src/components/SimpleGreeting/SimpleGreeting.ts
+++ src/components/SimpleGreeting/SimpleGreeting.ts
@@ -4,15 +4,18 @@ import styles from './SimpleGreeting.scss';
 
 @customElement('simple-greeting')
 export class SimpleGreeting extends LitElement {
-  // Define scoped styles right with your component, in plain CSS
   static override styles = [unsafeCSS(styles)];
 
-  // Declare reactive properties
   @property()
   name?: string = 'World';
 
-  // Render the UI as a function of component state
   override render() {
     return html`<p class="test">Hello, ${this.name}!</p>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'simple-greeting': SimpleGreeting;
   }
 }
